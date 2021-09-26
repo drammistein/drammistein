2127bdfbfeba2c1eb843753694e6a09698130e76
Daniel Ramos <drammistein@hotmail.com>
2021-09-25 22:11:23 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,91 @@
+let B7Validator = {
+    handleSubmit:(event)=> {
+        event.preventDefault() // Não vai deixar o evento padrão do envio pelo click no submit acontecer
+        let send = true;
+
+        let inputs = form.querySelectorAll('input');
+
+        // Função para limpar os erros depois de arrumado
+        B7Validator.clearErrors();
+
+        for(let i = 0; i < inputs.length; i++) {
+            let input = inputs[i];
+            let check = B7Validator.checkInput(input);
+
+            if(check !== true) {
+                send = false;
+                // Função para exibir o erro
+                // vai mostrar o input e o erro
+                B7Validator.showError(input, check); 
+            }
+        }
+
+        if(send) {
+            form.submit();
+        }
+    },
+    checkInput:(input)=> {
+        let rules = input.getAttribute('data-rules');
+
+        if(rules !== null) {
+            rules = rules.split('|');
+            for(let k in rules) {
+                let rDetails = rules[k].split('=');
+
+                switch(rDetails[0]) {
+                    case'required':
+                    if(input.value == '') {
+                        return 'Campo não pode ser vazio.';
+                    }
+                    break;
+                    case 'min':
+                        if(input.value.length < rDetails[1]) {
+                            return 'Campo tem que ter pelo menos '+rDetails[1]+' caractes';
+                        }
+                    break;
+                    case 'email':
+                        if(input.value |= '') {
+                            // regra para o e-mail
+                            let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                            if(!regex.test(input.value.toLowerCase()) ) {
+                                return 'E-mail digitado não é válido!';
+                            }
+                        }   
+                    break;
+                }
+            }
+        }
+
+        return true;
+    },
+
+    showError:(input, error) => {
+        // vai mostrar a borda de outra cor quando der o erro
+        input.style.borderColor = '#FF0000';
+        //vai criar a div onde vai ser mostrado o erro
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+
+        //Adicionar o erro depois do elemento
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
+    },
+
+    clearErrors:() => {
+        // Para remover as bordas das Divs que continham os erros
+        let inputs = form.querySelectorAll('input');
+        for(let i = 0; i < inputs.length; i++) {
+            inputs[i].style = '';
+        }
+
+        // Vai verificar todas as Divs com erros
+        let errorElements = document.querySelectorAll('.error');
+        // Loop para limpar os erros um por um 
+        for(let i = 0; i < errorElements.length; i++) {
+            errorElements[i].remove();
+        }
+    }
+};
+
+let form = document.querySelector('.b7validator');
+form.addEventListener('submit', B7Validator.handleSubmit);
